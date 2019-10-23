c9b00ad55006d77e2b3786243cc5e9cffdeeed5d
Fali <reyarcenegui@gmail.com>
2019-10-23 12:44:32 +0200

    interface goal improved

--- src/action_goal_interface_node.cpp
+++ src/action_goal_interface_node.cpp
@@ -3,28 +3,193 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <upo_actions/MakePlanAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <queue>
+#include <std_srvs/Trigger.h>
 
-upo_actions::MakePlanActionGoal actionGoal;
-
-bool goalRec = false;
-bool goalRunning = false;
-int n = 0;
-
-void goalCb(const geometry_msgs::PoseStampedConstPtr &goal)
+class MissionInterface
 {
-    ROS_INFO("Goal Interface: Sending action goal");
-    actionGoal.goal.global_goal = *goal;
-    actionGoal.goal_id.id = n++;
-    actionGoal.goal_id.stamp = ros::Time::now();
+    typedef actionlib::SimpleActionClient<upo_actions::MakePlanAction> MakePlanActionClient;
 
-    actionGoal.header.frame_id = "map";
-    actionGoal.header.seq = rand();
-    actionGoal.header.stamp = ros::Time::now();
+public:
+    MissionInterface()
+    {
+        makePlanClient.reset(new MakePlanActionClient("Make_Plan", false));
 
-    goalRec = true;
-    goalRunning = true;
-    
-}
+        nh.param("/mission_enabled", goals_by_file, (bool)1);
+
+        if (goals_by_file)
+        {
+            start_mission_server = nh.advertiseService("/start_mission", &MissionInterface::startMission, this);
+            reload_mission_data = nh.advertiseService("/reload_mission_data", &MissionInterface::reloadMissionData, this);
+            //TODO Load Here the file
+            missionLoaded = loadMissionData();
+        }
+        else
+        {
+            goal_sub = nh.subscribe<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1, &MissionInterface::goalCb, this);
+        }
+    }
+
+    void processMissions()
+    {
+        if (!makePlanClient->isServerConnected())
+        {
+            ROS_WARN("Make Plan Server disconnected! :(, Waiting again for the server...");
+            bool connected = makePlanClient->waitForServer(ros::Duration(10));
+            if (connected)
+            {
+                ROS_INFO("Make Plan Client and Server connected!");
+            }
+            else
+            {
+                ROS_WARN("Timeout waiting for server...retrying");
+            }
+        }
+        /**
+         * La idea es que se le van pasando golitos empezando desde el primero de la cola y cuando se recibe que se ha conseguido, se esperan unos
+         * ! segundos (O a que otro proceso tambien diga que palante, como por ejemplo algun nodo de inspeccion visual etc)
+         * y se le manda el siguiente y asi hasta el ultimo
+         * ! al llegar al ultimo se vuelve al estado inicial
+         * TODO Que pasa si alguno se cancela por el global planner.............
+         * 
+        **/
+        if (!goals_queu.empty() && !goalRunning)
+        {
+            actionGoal.goal.global_goal = goals_queu.front();
+            goals_queu.pop();
+            makePlanClient->sendGoal(actionGoal.goal);
+            goalRunning = true;
+        }
+
+        if(goals_queu.empty()){
+            ROS_INFO_THROTTLE(0.5,"No goals in the queu");
+        }
+        
+        if (goalRunning)
+        {
+            if (makePlanClient->getState() == actionlib::SimpleClientGoalState::ABORTED)
+            {
+                //?It can mean the robot need an operator or something else
+            }
+            
+            if (makePlanClient->getState() == actionlib::SimpleClientGoalState::LOST)
+            {
+                //!Maybe resend goal?
+            }
+            if(makePlanClient->getState() == actionlib::SimpleClientGoalState::PREEMPTED){
+                //!Do next goal?
+            }
+            if (makePlanClient->getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+            {
+                goalRunning = false;
+            }
+        }
+    }
+
+private:
+    void goalCb(const geometry_msgs::PoseStampedConstPtr &goal)
+    {
+
+        ROS_INFO("Goal Interface: Sending action goal");
+        actionGoal.goal.global_goal = *goal;
+        actionGoal.goal_id.id += 1;
+        actionGoal.goal_id.stamp = ros::Time::now();
+
+        actionGoal.header.frame_id = "map";
+        actionGoal.header.seq = rand();
+        actionGoal.header.stamp = ros::Time::now();
+
+        // goalRec = true;
+        // goalRunning = true;
+    }
+
+    bool startMission(std_srvs::TriggerRequest &req, std_srvs::TriggerResponse &resp)
+    {
+        if (!missionLoaded)
+        {
+            resp.success = false;
+            resp.message = "Couldn't start Mission because file was not loaded";
+        }
+        else
+        {
+            resp.success = true;
+            resp.message = "Starting Mission";
+            doMission = true;
+        }
+
+        return true;
+    }
+    bool reloadMissionData(std_srvs::TriggerRequest &req, std_srvs::TriggerResponse &resp)
+    {
+        resp.success = loadMissionData();
+
+        if (resp.success)
+        {
+            resp.message = "Mission Reloaded";
+        }
+        else
+        {
+            resp.message = "Couldn't reload mission";
+        }
+
+        return true;
+    }
+
+    bool loadMissionData()
+    {
+        goalsNbr = 0;
+
+        bool ret = true;
+        std::string base_path = "/mission/goal";
+        geometry_msgs::PoseStamped goal;
+
+        //TODO: Make frame id a parameter
+        goal.header.frame_id = "/map";
+
+        int i = 1;
+
+        while (nh.hasParam(base_path + std::to_string(i) + "/pose/x"))
+        {
+
+            nh.param(base_path + std::to_string(i) + "/pose/x", goal.pose.position.x, (double)0);
+            nh.param(base_path + std::to_string(i) + "/pose/y", goal.pose.position.y, (double)0);
+            nh.param(base_path + std::to_string(i) + "/orientation/x", goal.pose.orientation.x, (double)0);
+            nh.param(base_path + std::to_string(i) + "/orientation/y", goal.pose.orientation.y, (double)0);
+            nh.param(base_path + std::to_string(i) + "/orientation/z", goal.pose.orientation.z, (double)0);
+            nh.param(base_path + std::to_string(i) + "/orientation/w", goal.pose.orientation.w, (double)1);
+            goal.header.seq = i;
+            ROS_INFO("Goal %d (x,y)(x,y,z,w):\t[%.2f,%.2f]\t[%.2f,%.2f,%.2f,%.2f]", i, goal.pose.position.x, goal.pose.position.y, goal.pose.orientation.x, goal.pose.orientation.y, goal.pose.orientation.z, goal.pose.orientation.w);
+            goals_queu.push(goal);
+
+            ++goalsNbr;
+            ++i;
+        }
+
+        if (i == 1)
+        {
+            missionLoaded = false;
+        }
+        else
+        {
+            missionLoaded = true;
+        }
+        return missionLoaded;
+    }
+
+    bool missionLoaded = false;
+    bool doMission = false;
+
+    bool goalRunning = false;
+    bool goals_by_file;
+    int goalsNbr; //Number of goals loaded
+
+    upo_actions::MakePlanActionGoal actionGoal;
+    ros::NodeHandle nh;
+    std::queue<geometry_msgs::PoseStamped> goals_queu;
+    std::unique_ptr<MakePlanActionClient> makePlanClient; // makePlanClient("Make_Plan", false);
+    ros::ServiceServer start_mission_server, reload_mission_data;
+    ros::Subscriber goal_sub;
+};
 
 int main(int argc, char **argv)
 {
@@ -35,37 +200,15 @@ int main(int argc, char **argv)
 
     std::string node_name = "action_goal_interface_node";
     ros::init(argc, argv, node_name);
-    ros::NodeHandle n;
 
-    actionlib::SimpleActionClient<upo_actions::MakePlanAction> makePlanClient("Make_Plan", false);
+    MissionInterface iface;
 
-    ros::Subscriber goal_sub = n.subscribe("/move_base_simple/goal", 1, goalCb);
-    
     ros::Rate sleep_rate(10);
+
     while (ros::ok())
     {
         ros::spinOnce();
-        if (goalRec)
-        {
-            makePlanClient.sendGoal(actionGoal.goal);
-            goalRec = false;
-        }
-
-        
-        if (goalRunning)
-        {
-            ROS_INFO_THROTTLE(1, "Status: %s", makePlanClient.getState().toString().c_str());
-            if (makePlanClient.getState().isDone()){
-                goalRunning = false;
-                ROS_INFO("Done! :)");
-            }
-                
-        }
-        //if (!makePlanClient.isServerConnected())
-        //{
-        //    ROS_INFO("Server disconnected! :(, Waiting again for the server...");
-        //    makePlanClient.waitForServer();
-        //}
+        iface.processMissions();
 
         sleep_rate.sleep();
     }
