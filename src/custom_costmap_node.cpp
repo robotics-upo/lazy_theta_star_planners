b8e70f05f0d4c416a494981ec5905e86a12ae376
Fali <reyarcenegui@gmail.com>
2019-10-28 12:46:00 +0100

    uncomment #define MELODIC for melodic compiling

--- src/custom_costmap_node.cpp
+++ src/custom_costmap_node.cpp
@@ -6,8 +6,11 @@
 
 #include <std_srvs/Trigger.h>
 #include <tf/transform_listener.h>
+#include <tf2_ros/transform_listener.h>
 
-costmap_2d::Costmap2DROS *costmap_ptr;
+//#define MELODIC 
+
+std::unique_ptr<costmap_2d::Costmap2DROS> costmap_ptr;
 
 bool resetCostmapSrv(std_srvs::TriggerRequest &req, std_srvs::TriggerResponse &rep){
 
@@ -19,14 +22,19 @@ int main(int argc, char** argv){
 
     
     ros::init(argc,argv,"custom_costmap_node");
-    
     ros::NodeHandle n("~");
     ros::ServiceServer reset_costmap_svr = n.advertiseService("reset_costmap", resetCostmapSrv);
 
-    tf::TransformListener tfListener(ros::Duration(5));
-    costmap_2d::Costmap2DROS costmap("costmap", tfListener);
-    costmap_ptr = &costmap;
-    
+    #ifdef MELODIC
+    tf2_ros::Buffer buffer(ros::Duration(5));
+    tf2_ros::TransformListener tf(buffer);
+    costmap_ptr.reset(new costmap_2d::Costmap2DROS("costmap", buffer));
+    #endif
+    #ifndef MELODIC
+    tf::TransformListener tfListener(ros::Duration(5));    
+    costmap_ptr.reset(new costmap_2d::Costmap2DROS("costmap", tfListener));
+    #endif
+
     ros::spin();
     
     return 0;
