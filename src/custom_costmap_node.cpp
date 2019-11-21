fe9d0a7812824aebe6be84febd2e8f0948edc4f5
Fali <reyarcenegui@gmail.com>
2019-11-21 11:24:22 +0100

    using inscribed inflated obstacle value to chk env

--- src/custom_costmap_node.cpp
+++ src/custom_costmap_node.cpp
@@ -7,10 +7,13 @@
 #include <std_srvs/Trigger.h>
 #include <tf/transform_listener.h>
 #include <tf2_ros/transform_listener.h>
+#include <visualization_msgs/Marker.h>
 
 std::unique_ptr<costmap_2d::Costmap2DROS> costmap_ptr;
 typedef unsigned int uint;
 double x_b,y_b,n_max;
+std::string robot_frame;
+visualization_msgs::Marker markers;
 
 bool resetCostmapSrv(std_srvs::TriggerRequest &req, std_srvs::TriggerResponse &rep){
 
@@ -33,9 +36,9 @@ bool checkEnvSrv(std_srvs::TriggerRequest &req, std_srvs::TriggerResponse &rep){
 
     for(uint i = (s_x-x1); i < (s_x+x2); ++i)
         for(uint j = (s_y-y1); j < (s_y+y2); ++j)
-            if(costmap_ptr->getCostmap()->getCost(i,j) == costmap_2d::LETHAL_OBSTACLE)
+            if(costmap_ptr->getCostmap()->getCost(i,j) == costmap_2d::INSCRIBED_INFLATED_OBSTACLE)
                 ++count;
-
+    ROS_INFO("Sx: %d\t Sy: %d\t x1: %d\t x2: %d\t y1: %d\t y2: %d", s_x,s_y,x1,x2,y1,y2);
     if(count > n_max){
         rep.message="Too much obstacles: " + std::to_string(count);
         rep.success=false;
@@ -46,6 +49,38 @@ bool checkEnvSrv(std_srvs::TriggerRequest &req, std_srvs::TriggerResponse &rep){
 
     return true;
 }
+void configMarkers(){
+
+   markers.points.resize(0);
+    
+    markers.header.frame_id = robot_frame;
+    markers.header.stamp = ros::Time();
+    markers.ns = "local_costmap";
+    markers.id = 1;
+    markers.type = visualization_msgs::Marker::LINE_STRIP;
+    markers.action = visualization_msgs::Marker::ADD;
+    markers.lifetime = ros::Duration(100000);
+    markers.scale.x = 0.1;
+    markers.color.a = 1.0;
+    markers.color.b = 1.0;
+    markers.color.g = 1.0;
+    markers.color.r = 0.0;
+    markers.pose.orientation.w=1;
+
+    geometry_msgs::Point p;
+    p.x = x_b;
+    p.y = y_b;
+    markers.points.push_back(p);
+    p.y=-y_b;
+    markers.points.push_back(p);
+    p.x=-x_b;
+    p.y=-y_b;
+    markers.points.push_back(p);
+    p.y=y_b;
+    markers.points.push_back(p);
+    p.x = x_b;
+    markers.points.push_back(p);
+}
 int main(int argc, char** argv){
 
     
@@ -55,10 +90,13 @@ int main(int argc, char** argv){
     n.param("x_bound", x_b,(double)0.65);
     n.param("y_bound", y_b,(double)0.4);
     n.param("n_max", n_max, (double)20);
-
+    n.param("robot_frame", robot_frame, (std::string)"siar/base_link");
+    
     ros::ServiceServer reset_costmap_svr = n.advertiseService("reset_costmap", resetCostmapSrv);
     ros::ServiceServer check_env = n.advertiseService("check_env", &checkEnvSrv);
-
+    ros::Publisher markers_pub = n.advertise<visualization_msgs::Marker>("rotation_check_markers", 2);
+    configMarkers();
+    
     #ifdef MELODIC
     tf2_ros::Buffer buffer(ros::Duration(5));
     tf2_ros::TransformListener tf(buffer);
@@ -68,7 +106,15 @@ int main(int argc, char** argv){
     tf::TransformListener tfListener(ros::Duration(5));    
     costmap_ptr.reset(new costmap_2d::Costmap2DROS("costmap", tfListener));
     #endif
-    ros::spin();
+    ros::Rate loop_rate(10);
+    while(ros::ok()){
+        ros::spinOnce();
+        if( markers_pub.getNumSubscribers() > 0){
+            markers_pub.publish(markers);
+        }
+        loop_rate.sleep();
+    }
+   
     
     return 0;
 }
