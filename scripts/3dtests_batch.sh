4db24e1c8a6ac76a8d59469de0fe0e86577a58d8
Rafael Rey <reyarcenegui@gmail.com>
2020-11-16 16:38:00 +0100

    Added the possibility of launching tests multiple times

--- scripts/3dtests_batch.sh
+++ scripts/3dtests_batch.sh
@@ -8,7 +8,7 @@ cost_w=$3
 map=$4
 initial_point="102 106 3"
 goal_point="103 106 3" 
-
+n_tries=$5
 timeout=350
 
 map_path=$(rospack find theta_star_2d)/$map
@@ -21,25 +21,29 @@ roslaunch theta_star_2d test_3d_costmap.launch timeout:=$timeout batch_mode:=tru
 sleep 30
 for line in $lof; do
     for cost in $cost_w; do
-        echo "cost: $cost, line of sight: $line, timeout: $timeout , map: $map"
-        #Launch here the stuff
-        rosrun dynamic_reconfigure dynparam set /global_planner_node goal_weight $cost
-        rosrun dynamic_reconfigure dynparam set /global_planner_node lof_distance $line
-        #Call service blocking to get map and stuff
-        rosservice call --wait /global_planner_node/get_path "start:
+      for (( n=1; n<=$n_tries; n++ ))
+      do
+          echo "Try number $n / $n_tries"
+          echo "cost: $cost, line of sight: $line, timeout: $timeout , map: $map"
+          #Launch here the stuff
+          rosrun dynamic_reconfigure dynparam set /global_planner_node goal_weight $cost
+          rosrun dynamic_reconfigure dynparam set /global_planner_node lof_distance $line
+          #Call service blocking to get map and stuff
+          rosservice call --wait /global_planner_node/get_path "start:
   x: ${initial_array[0]}
   y: ${initial_array[1]}
   z: ${initial_array[2]}
 goal:
   x: ${goal_array[0]}
   y: ${goal_array[1]}
-  z: ${goal_array[2]}" >> ~/3dtest/"$map"_lof_"$line"_cw_"$cost".txt
+  z: ${goal_array[2]}" >> ~/3dtest/"$map"_lof_"$line"_cw_"$cost"_$n.txt
 
-        test_name="$map"_lof_"$line"_cw_"$cost".png
-        echo "$test_name"
-        #Gnome-screenshot
-        gnome-screenshot -f ~/3dtest/$test_name
-        spd-say 'Test done'
+          test_name="$map"_lof_"$line"_cw_"$cost"_$n.png
+          echo "$test_name"
+          #Gnome-screenshot
+          gnome-screenshot -f ~/3dtest/$test_name
+          spd-say 'Test done'
+      done
     done
 done
 
